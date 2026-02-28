43aa7e4c0a10c2cdbcd73b2c7c5be19391556920
Rubel <mr.adcadre41@gmail.com>
2026-02-28 11:44:03 +0600

    cel to fahrenheit added

---
+++ randomNUmGenrtr.js
@@ -0,0 +1,3 @@
+// Generate a random number between 10 to 20.
+let ranNum = Math.random() * (20 - 10 + 1) + 10;
+console.log(Math.floor(ranNum));
--- task-1.js
+++ task-1.js
@@ -1,1 +1,7 @@
 //Task-1:Write a function to convert temperature from Celsius to Fahrenheit.
+function celToFarCon(celsius) {
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
+}
+const celValue = celToFarCon(30);
+console.log(celValue);
